175c8c1e507eab4bafe5f1c7939343e931031cd9
piyush <piyushgoel997@gmail.com>
2020-12-30 18:06:43 -0500

    add inner expectation

--- Classifier.py
+++ Classifier.py
@@ -11,7 +11,7 @@ from Utils import argmax, calc_uncertainty, argmin
 
 
 class Classifier:
-    def __init__(self, type, categorical=None, imputer_type="mf"):
+    def __init__(self, type, categorical=None, imputer_type="mf", uncertainty_measure="confidence"):
         if type == "decision_tree" or type == "dt":
             self._clf = DecisionTreeClassifier()
         elif type == "neural_network" or type == "nn":
@@ -26,14 +26,17 @@ class Classifier:
 
         self._random_forests = None
         self._categorical = categorical
+        self._uncertainty_measure = uncertainty_measure
+        self._alpha = 0.5
 
     def train(self, X, Y, incomplete=True):
         _X = np.copy(X)
-        self.imputer.fit(_X, y=Y, cat_vars=self._categorical)
+        self.imputer.fit(_X, cat_vars=self._categorical)
         if incomplete:
             _X = self._impute_missing(_X)
         else:
             self._train_rfs(_X, _X)
+        self._alpha = np.sum(Y) / len(Y)
         return self._clf.fit(_X, Y)
 
     def next_features(self, X, method):
@@ -45,8 +48,8 @@ class Classifier:
         # while calculating for one unknown feature I use the imputed value for the other unknown features.
         next_features = []
         if method is "random":
-            for x in X:
-                p = [a[0] for a in np.argwhere(~np.isnan(x))]
+            for _x in X:
+                p = [a[0] for a in np.argwhere(~np.isnan(_x))]
                 try:
                     next_features.append(random.choice(p))
                 except:
@@ -66,12 +69,13 @@ class Classifier:
                         num = 0
                         den = 0
                         for c, v in f_counts.items():
-                            x = _X[i, :]
+                            _x = _X[i, :]
+                            _x[j] = c
+                            x = X[i, :]
                             x[j] = c
-                            # TODO instead of p do E[p|j,Known features] here
-                            # expected_p = self._expected_prob(x)
-                            cls_probs = self.predict(x.reshape((1, len(x))), incomplete=False)[0]
-                            num += v * calc_uncertainty(cls_probs)
+                            expected_p = self._expected_prob(_x, x)
+                            # cls_probs = self.predict(_x.reshape((1, len(_x))), incomplete=False)[0]
+                            num += v * calc_uncertainty(expected_p, method=self._uncertainty_measure, alpha=self._alpha)
                             den += v
                         expected_uncert_matrix[i, j] = num / den
             next_features = [argmin(x) for x in expected_uncert_matrix]
@@ -130,6 +134,16 @@ class Classifier:
             rfs.append(clf)
         self._random_forests = rfs
 
-    # def _expected_prob(self, x):  # TODO
-    #     # impute randomly for the other features
-
+    def _expected_prob(self, _x, x):
+        # impute randomly for the other features
+        _X = np.zeros((100, len(x)))  # TODO
+        for j in range(_X.shape[1]):
+            if not np.isnan(x[j]):
+                continue
+            _x_minus_j = np.concatenate([_x[:j], _x[j + 1:]])
+            for i in range(_X.shape[0]):
+                _X[i, j] = self._random_forests[j].estimators_[
+                    random.randint(0, self._random_forests[j].n_estimators - 1)].predict(
+                    _x_minus_j.reshape((1, len(_x_minus_j))))
+        pred = self.predict(_X, incomplete=False)
+        return np.average(pred, axis=0)
