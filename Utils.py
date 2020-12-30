b9f148439a34f5ea2aa3bb6772c73d8f726427cd
piyush <piyushgoel997@gmail.com>
2020-12-30 18:06:15 -0500

    add different measures of uncertainty

--- Utils.py
+++ Utils.py
@@ -1,3 +1,6 @@
+import math
+
+
 def argmax(l):
     return max(enumerate(l), key=lambda x: x[1])[0]
 
@@ -6,5 +9,16 @@ def argmin(l):
     return min(enumerate(l), key=lambda x: x[1])[0]
 
 
-def calc_uncertainty(predictions):
-    return 1 - max(predictions)
+def calc_uncertainty(predictions, alpha=0.5, method="confidence"):
+    p = predictions[1]
+    p = math.pow(p, -math.log(2, alpha))
+    uncert = 1
+    if method is "confidence":
+        uncert = 1 - abs(1 - 2 * p)
+    elif method is "variance":
+        uncert = p * (1 - p)
+    elif method is "entropy":
+        uncert = p * math.log(p) + (1 - p) * math.log(1 - p)
+    else:
+        raise ValueError("Incorrect method")
+    return uncert
