f294bd72f8e87202503e90c511b023661e49c411
piyush <piyushgoel997@gmail.com>
2020-12-31 16:29:03 -0500

    update script creator

--- CreateBashScript.py
+++ CreateBashScript.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
 
     names = set([str(f).split(".")[0]
                  for f in listdir(path) if isfile(join(path, f)) and "log" not in f and "fc" not in f])
-    commands = "\n".join(["python Main.py --data " + n + " &" for n in names])
+    c1 = [" --data " + str(n) for n in names]
+    c2 = [" --um " + um for um in ["confidence", "variance", "entropy", "confidence*", "variance*", "entropy*"]]
+    c3 = [" --clf " + clf for clf in ["nn", "dt"]]
+    commands = "\n".join(["python Main.py" + a + b + c + " &" for a in c1 for b in c2 for c in c3])
     f = open("run_all", 'w')
     f.write(commands)
