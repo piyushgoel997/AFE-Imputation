e1f447746b10b1c847d6cf365f6bd5b575528e60
piyush <piyushgoel997@gmail.com>
2021-01-09 22:31:09 -0500

    reduce the memory requested, make more jobs short

--- CreateBashScript.py
+++ CreateBashScript.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     except:
         print()
 
-    memory = {"abalone_0": "50G", "adult_0": "600G", "avila_0": "200G", "bank_0": "600G", "biodeg_0": "100G",
-              "cardiotocography_0": "100G", "car_0": "10G", "credit_card_defaulters_0": "500G",
-              "drug_consumption_5": "50G", "faults_0": "100G", "frogs_0": "200G", "mushroom_0": "100G",
-              "obesity_0": "50G", "online_shoppers_intention_0": "200G", "phishing_0": "10G", "sat_0": "300G",
-              "Sensorless_drive_diagnosis_0": "2500G", "shill_bidding_0": "100G", "spambase_0": "500G",
-              "statlog-is_0": "50G", "statlog-ls_0": "100G", "wine_quality_merged_0": "100G"}
+    memory = {"abalone_0": "10G", "adult_0": "100G", "avila_0": "50G", "bank_0": "100G", "biodeg_0": "10G",
+              "cardiotocography_0": "20G", "car_0": "5G", "credit_card_defaulters_0": "100G",
+              "drug_consumption_5": "10G", "faults_0": "20G", "frogs_0": "50G", "mushroom_0": "20G",
+              "obesity_0": "10G", "online_shoppers_intention_0": "50G", "phishing_0": "5G", "sat_0": "300G",
+              "Sensorless_drive_diagnosis_0": "500G", "shill_bidding_0": "20G", "spambase_0": "100G",
+              "statlog-is_0": "10G", "statlog-ls_0": "20G", "wine_quality_merged_0": "20G"}
     num_features = [8, 13, 10, 16, 41, 25, 6, 23, 12, 27, 20, 22, 16, 17, 9, 36, 100, 9, 57, 19, 36, 12]
 
     c2 = ["confidence", "variance", "entropy", "confidence*", "variance*", "entropy*"]
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     for a, mem in [x for _, x in sorted(zip(num_features, memory.items()), key=lambda item: item[0])]:
         for b in c2:
             for c in c3:
-                if "adult" in a:
+                if "statlog" in a:
                     partition = "long"
                     time = "5-00:00"
                 n = a + "_" + b[:-6] + "_" + c
