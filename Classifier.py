corrected average uncertainty

Classifier.py
@@ -117,8 +117,9 @@ class Classifier:
         return np.average(pred, axis=0)
 
     def average_uncertainty(self, X):
-        pred = self.predict(X)
         uncert = 0
-        for p in pred:
-            uncert += calc_uncertainty(p, alpha=self._alpha, method=self._uncertainty_measure)
-        return uncert / len(pred)
+        for k in range(self._num_imputers):
+            for i in range(X.shape[0]):
+                expected_p = self._expected_prob(np.copy(X[i]), k)
+                uncert += calc_uncertainty(expected_p, method=self._uncertainty_measure, alpha=self._alpha)
+        return uncert / (len(X) * self._num_imputers)

Main.py
@@ -99,7 +99,7 @@ class Exp:
             clf.train(X_train, Y_train)
             ai, ari, ui, si = self.run_exp(X_test, X_test_complete, Y_test, clf)
             print("Incomplete data finished in", time.time() - t1)
-            print(rr, ac, arc, sc, ai, ari, si)
+            print(rr, ac, arc, uc, sc, ai, ari, ui, si)
             acc_exp[j] = np.array(ac + ai)
             auc_exp[j] = np.array(arc + ari)
             uncert_exp[j] = np.array(uc + ui)